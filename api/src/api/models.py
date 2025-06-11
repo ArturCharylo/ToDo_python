f5a7871ca9e5698733aebbf9179819ffe54104b0
takojakos <dziki.sledz@op.pl>
2025-06-11 23:05:23 +0200

    Migrated data from data.json to sglite database and created a table Task

--- api/src/api/models.py
+++ api/src/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    deadline = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ['-id']  # Order by ID descending
--- api/src/api/urls.py
+++ api/src/api/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.ApiPage.as_view(), name='api'),
-    path('hello/', views.HelloView.as_view(), name='hello'),
 ]
