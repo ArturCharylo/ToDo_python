d7ff156667fc754360f2a9f76f7f6d3afbd3672a
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-02-23 20:11:46 +0100

    refactor: cleaned up serializers.py

--- api/src/api/serializers.py
+++ api/src/api/serializers.py
@@ -1,15 +1,8 @@
-# shop/serializers.py
 from rest_framework import serializers
-from .models import Task, User
+from .models import Task
 
-
+# Serializer that automatically handles converting Task instances to JSON and vice-versa
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
         fields = '__all__'
